0f945776eef075d5eeb7b7e55d7bc59d2649436d
Pawel1894 <pawepohl@gmail.com>
2023-09-22 20:06:23 +0200

    extract bearer function typesafe improvement

--- api/libs/shared/src/utils/extract-bearer-token.util.ts
+++ api/libs/shared/src/utils/extract-bearer-token.util.ts
@@ -1,4 +1,6 @@
-export function extractBearerTokenFromAuthHeader(authHeader: string) {
+export function extractBearerTokenFromAuthHeader(authHeader: string | null) {
+	if (!authHeader || typeof authHeader !== "string") return null;
+
 	const authHeaderParts = authHeader.split(" ");
 	if (authHeaderParts[0]?.toLowerCase() !== "bearer") return null;
 	const [, jwt] = authHeaderParts;
